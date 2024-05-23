dde2aaa06f32a3916ded83ce59627d9bd4ef9083
wwckd <123295180+wwckd@users.noreply.github.com>
2024-05-23 03:59:49 -0700

    replace results page

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("Results")
 st.write("Here you can put each of your key results.")
+
+st.image("../images/total_spending.png")
+st.text("Travel is the top spender's highest spending, followed by onsite grocery and onsite shopping.")
